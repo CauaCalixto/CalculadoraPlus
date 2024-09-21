5f2f087beaa034d47967be7ce89a392b30ce0830
Cauã <cauacalixto.almeida@gmail.com>
2024-09-21 14:05:09 -0300

    Adiciona formas geométricas ao projeto CalculadoraPlus
    Inclui funções para cálculo de áreas de triângulo, círculo, quadrado, retângulo, trapézio, losango, paralelogramo e hexágono.

--- calculadora.py
+++ calculadora.py
@@ -5,12 +5,14 @@ def boas_vindas():
     """
     Exibe uma mensagem de boas-vindas e opções iniciais.
     """
-    print("+-------------------------------+")
-    print("|     BEM-VINDO(A) À CALCULADORA |")
-    print("+-------------------------------+")
-    print("|     1. Calculadora Científica  |")
-    print("|     2. Calculadora Simples     |")
-    print("+-------------------------------+")
+    print("+-------------------------------------------+")
+    print("|     BEM-VINDO(A) À CALCULADORA            |")
+    print("+-------------------------------------------+")
+    print("|     1. Calculadora Científica             |")
+    print("|     2. Calculadora Simples                |")
+    print("|     3. Calculadora Formas Geometricas     |")
+    print("|     4. Sair                               |")
+    print("+-------------------------------------------+")
 
 def mostrar_menu_cientifica():
     """
@@ -42,6 +44,70 @@ def mostrar_menu_calculadora_simples():
     | 5. Voltar ao Menu Principal     |
     ─────────────────────────────────
     ''')
+def mostrar_menu_formas_geometricas():
+    '''Exibindo o menu de formas geometricas'''
+    print('''
+─────────────────────────────────
+    |     Formas Disponiveis        |
+    ─────────────────────────────────
+    | 1. Triângulo                  |
+    | 2. Círculo                    |
+    | 3. Quadrado                   |
+    | 4. Retângulo                  |
+    | 5. Trapézio                   |
+    | 6. Losango                    |
+    | 7. Parelelogramo              |
+    | 8. Héxagono regular           |
+    | 9. Sair                       |
+    ─────────────────────────────────''')
+def calcular_triângulo(base, altura):
+    area = base * altura / 2
+    print("Realizando operação...")
+    time.sleep(0.5)
+    return area
+
+def calcular_circulo(raio):
+    area = math.pi * raio ** 2
+    print("Realizando Operação...")
+    time.sleep(0.5)
+    return area
+
+def calcular_quadrado(lado):
+    area = lado * lado * 2
+    print("Realizando operação...")
+    time.sleep(0.5)
+    return area
+
+def calcular_retangulo(base, altura):
+    area = base * altura
+    print("Realizando operação")
+    time.sleep(0.5)
+    return area
+
+def calcular_trapezio(base_maior, base_menor, altura):
+    area = (base_maior + base_menor) * altura / 2
+    print("Realizando Operação...")
+    time.sleep(0.5)
+    return area
+
+def calcular_losango(diagonal_maior, diagonal_menor):
+    area = diagonal_maior * diagonal_menor / 2
+    print("Realizando operação...")
+    time.sleep(0.5)
+    return area
+
+def calcular_paralelograma(base, altura):
+    area = base * altura
+    print("Realizando operação....")
+    time.sleep(0.5)
+    return area
+
+def calcular_hexagono(lado):
+    area = (3 * math.sqrt(3) * lado ** 2) / 2
+    print("Realizando operação....")
+    time.sleep(0.5)
+    return area
+
 
 def calcular_seno(numero):
     print("Realizando a operação...")
@@ -80,6 +146,7 @@ def calculadora_cientifica():
                     elif escolha_funcao == 3:
                         resultado = calcular_tangente(numero)
                         print(f"Após realizar o cálculo de {numero}, a tangente é: {resultado:.4f}")
+                    
                 except ValueError:
                     print("Número inválido. Por favor, insira um número válido.")
             else:
@@ -127,6 +194,57 @@ def calculadora_simples():
         except ValueError:
             print("Opção inválida. Por favor, insira um valor inteiro.")
 
+def formas_geometricas():
+    '''Gerenciando as formas geometricas'''
+    while True:
+        try:
+            mostrar_menu_formas_geometricas()
+            escolher_opcao = int(input("Selecione uma opção: "))
+            if escolher_opcao == 9:
+                break # Voltando ao menu 
+            elif escolher_opcao == 1:
+                base = float(input("Base: "))
+                altura = float(input("Altura: "))
+                print(f"Àrea do Triângulo é: {calcular_triângulo(base, altura)}")
+
+            elif escolher_opcao == 2:
+                raio = float(input("Raio: "))
+                print(f"Àrea do circulo:{calcular_circulo(raio)}")
+
+            elif escolher_opcao == 3:
+                lado = int(input("Quantidade de lado: "))
+                print(f"Perímetro: {calcular_quadrado(lado)}")
+
+            elif escolher_opcao == 4:
+                base = float(input("Base: "))
+                altura = float(input("Altura: "))
+                print(f"Àrea do Retângulo é: {calcular_retangulo(base, altura)}") 
+
+            elif escolher_opcao == 5:
+                base_maior = float(input("Base maior: "))
+                base_menor = float(input("Base menor: "))
+                altura = float(input("Altura: "))
+                print(f"Àrea: {calcular_trapezio(base_maior, base_menor, altura)}")
+            
+            elif escolher_opcao == 6:
+                diagonal_maior = float(input("Diagonal maior: "))
+                diagonal_menor = float(input("Diagonal menor: "))
+                print(f"Àrea: {calcular_losango(diagonal_maior, diagonal_menor)}")
+            
+            elif escolher_opcao == 7:
+                base = float(input("Base: "))
+                altura = float(input("Altura: "))
+                print(f"Área: {calcular_paralelograma(base, altura)}")
+            
+            elif escolher_opcao == 8:
+                lado = float(input("Lado: "))
+                print(f"Área: {calcular_hexagono(lado)}")     
+            else:
+                    print("Opção inválida")
+
+        except ValueError:
+            print("Opção inválida")
+
 def escolher_opcao():
     """
     Permite ao usuário escolher entre calculadora científica ou simples.
@@ -139,6 +257,11 @@ def escolher_opcao():
                 calculadora_cientifica()
             elif opcao == 2:
                 calculadora_simples()
+            elif opcao == 3:
+                formas_geometricas()
+            elif opcao == 4:
+                print("Até Logo...")
+                break # Finalizando Programa
             else:
                 print("Opção inválida. Tente novamente.")
         except ValueError:
